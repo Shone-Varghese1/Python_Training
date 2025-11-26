e571520316bb166c8361ddcccd038bc3dc5cf26e
Shone-Varghese1 <shone.varghese@gds.ey.com>
2025-11-26 17:47:29 +0530

    added shopping cart file

---
+++ 26-11-2025/class_shopping_cart.py
@@ -0,0 +1,52 @@
+class ShoppingCart:
+
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, name, price):
+        self.items.append((name, price))
+        print(name, "added to cart")
+
+    def remove_item(self, name):
+        for item in self.items:
+            if item[0] == name:
+                self.items.remove(item)
+                print(name, "removed from cart")
+                return
+        print("item not found in cart")
+
+    def total_cost(self):
+        total= 0
+        for item in self.items:
+            total += item[1]
+        return total
+
+    def apply_discount(self, percent):
+        total= self.total_cost()
+        discount_amount = total * (percent / 100)
+        final_price = total - discount_amount
+        return final_price
+
+    def display_items(self):
+        if not self.items:
+            print("No items in the cart")
+            return
+
+        print("Items in cart:")
+        for name, price in self.items:
+            print(name, "-", price)
+
+
+cart = ShoppingCart()
+cart.add_item("laptop", 50000)
+cart.add_item("mobile", 30000)
+cart.add_item("mouse", 5000)
+print()
+cart.display_items()
+print()
+print("Total cost:", cart.total_cost())
+print("Final price after discount:", cart.apply_discount(10))
+print()
+cart.remove_item("laptop")
+cart.display_items()
+
